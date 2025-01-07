c2431c6a1fbac245f778796f0c1b63331ae44c50
Loki Ng <dev@lokikl.com>
2025-01-07 18:21:22 +0800

    untitled updates

--- nvim.d/my/lsp/init.lua
+++ nvim.d/my/lsp/init.lua
@@ -34,13 +34,13 @@ require("lspconfig").ruff_lsp.setup {
   on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format()
-				end,
-			})
+			-- vim.api.nvim_create_autocmd("BufWritePre", {
+			-- 	group = augroup,
+			-- 	buffer = bufnr,
+			-- 	callback = function()
+			-- 		vim.lsp.buf.format()
+			-- 	end,
+			-- })
 		end
 	end,
 }
